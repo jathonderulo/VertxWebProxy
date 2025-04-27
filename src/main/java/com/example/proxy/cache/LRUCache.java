2f55cf181f07f5d10710000146cad62e570002cb
jathonderulo <119631105+jathonderulo@users.noreply.github.com>
2025-04-28 00:45:13 +0100

    Finish LRUCache implementation

--- src/main/java/com/example/proxy/cache/LRUCache.java
+++ src/main/java/com/example/proxy/cache/LRUCache.java
@@ -1,17 +1,23 @@
 package com.example.proxy.cache;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class LRUCache {
     private final Map<String, String> requestToResponseMap = new HashMap<>();
+    private final Queue<String> accessOrderQueue = new ArrayDeque<>();
+    private int maxSize;
 
-    public LRUCache() {
-
+    public LRUCache(int size) {
+        this.maxSize = size;
     }
 
     public synchronized void addEntry(String request, String response) {
+        if(requestToResponseMap.size() == maxSize) {
+            System.out.println("Max size reached.");
+            evictLRUEntry();
+        }
 
+        requestToResponseMap.put(request, response);
     }
 
     public synchronized boolean hasRequest(String request) {
@@ -19,7 +25,6 @@ public class LRUCache {
         if (requestToResponseMap.containsKey(request)) {
             return true;
         }
-
         return false;
     }
 
@@ -29,6 +34,15 @@ public class LRUCache {
 
     public synchronized void printStatistics(String request) {
         // TODO: Print all the statistics
+    }
+
+    private void evictLRUEntry() {
+        String itemToRemove = accessOrderQueue.poll();
+        if (requestToResponseMap.remove(itemToRemove) == null) {
+           System.out.println("Failed to evict an entry.");
+        } else {
+            System.out.println("Evicted an entry.");
+        }
     }
 }
 
---
+++ src/test/java/com/example/proxy/cache/LRUCacheTest.java
@@ -0,0 +1,7 @@
+package com.example.proxy.cache;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LRUCacheTest {
+}
